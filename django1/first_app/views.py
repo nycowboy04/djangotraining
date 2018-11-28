75c093aaad32187a5d34ea0c91aa44d6b231005c
Matthew Sheinberg <msheinberg@gmail.com>
2018-11-28 13:29:47 -0500

    Injected database data into webpage.

--- django1/first_app/views.py
+++ django1/first_app/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Topic, Webpage, AccessRecord
 
 def index (request):
+    webpages_list = AccessRecord.objects.order_by('date')
+    date_dict={'access_records':webpages_list}
     my_dict = {'insert_me':"Hello I am from views.py"}
-    return render(request, 'first_app/index.html',context=my_dict)
+    return render(request, 'first_app/index.html',context=date_dict)
 # Create your views here.
