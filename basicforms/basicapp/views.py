9df8a7149f8c4f82092021c82b79e4b43c8c3330
Matthew Sheinberg <msheinberg@gmail.com>
2018-11-29 12:54:52 -0500

    whatever

--- basicforms/basicapp/views.py
+++ basicforms/basicapp/views.py
@@ -1,20 +1,18 @@
 from django.shortcuts import render
-from . import forms
+#from . import forms
+from AppTwo.forms import UserForm
 
 def index(request):
     return render(request,'basicapp/index.html')
 
-def form_name_view(request):
-    form=forms.FormName()
-
-    if request.method=='post':
-        form=forms.FormName(request.POST)
+def users(request):
+    form = UserForm()
+    if request.method == 'POST':
+        form = UserForm(request.POST)
 
         if form.is_valid():
-            print("VALIDATION SUCCESS")
-            print("Name: "+form.cleaned_data['name'])
-            print("Email: "+form.cleaned_data['email'])
-            print("Text: "+form.cleaned_data['text'])
-
-    return render(request,'basicapp/form_page.html', {'form':form})
-# Create your views here.
+            form.save(commit=True)
+            return index(request)
+        else:
+            print("Error form invalid")
+    return render(request,'apptwo/users.html', {'form':form})
