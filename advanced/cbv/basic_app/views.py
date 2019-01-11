b04371e84166a132836c3fffe1eb364433590134
Matthew Sheinberg <matthewsheinberg@MSheinberg.local>
2019-01-11 11:53:40 -0500

    Created a generic template view and injected content into it using a dictionary

--- advanced/cbv/basic_app/views.py
+++ advanced/cbv/basic_app/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.views.generic import View, TemplateView
 
 # Create your views here.
+#def index(request):
+#    return render(request, 'index.html')
+class IndexView(TemplateView):
+    template_name='index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['injectme']='basic injection!'
+        return context
