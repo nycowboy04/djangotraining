c03208b251926aec780d1c4d4ed574e81df9c1bf
Matthew Sheinberg <matthewsheinberg@MSheinberg.local>
2019-01-15 08:17:20 -0500

    Linted forms.py

--- mysite/blog/forms.py
+++ mysite/blog/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from blog.models import Post, Comment
 
+
 class PostForm(forms.ModelForm):
 
     class Meta():
@@ -8,18 +9,18 @@ class PostForm(forms.ModelForm):
         fields = ('author', 'title', 'text')
 
         widgets = {
-            'title':forms.TextInput(attrs={'class':'textinputclass'}),
-            'text':forms.TextArea(attrs={'class':'editable medium-editor-textarea postcontent'})
+            'title': forms.TextInput(attrs={'class': 'textinputclass'}),
+            'text': forms.TextArea(attrs={'class': 'editable medium-editor-textarea postcontent'}),
         }
 
 
-class CommentForm(forms.ModelForm)
+class CommentForm(forms.ModelForm):
 
     class Meta():
         model = Comment
         fields = ('author', 'text')
 
         widgets = {
-        'author':forms.TextInput(attrs={'class':'textinputclass'}),
-        'text':forms.TextArea(attrs={'class':'editable medium-editor-textarea'})
+            'author': forms.TextInput(attrs={'class': 'textinputclass'}),
+            'text': forms.TextArea(attrs={'class': 'editable medium-editor-textarea'})
         }
